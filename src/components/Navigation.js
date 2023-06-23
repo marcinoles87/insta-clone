d6c3de8c0af2fd357e8724517b6039651457e3c0
marcinoles87 <marcinoles87@gmail.com>
2023-06-23 05:30:55 +0200

    add nav components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
+import logo from '../img/Instagram.png';
+import search from '../img/search.jpg'
+
 function Navigation() {
   return (
     <nav className='nav-container'>
         <div className='nav-logo'>
-            <img className='logo' src='./img/Instagram.png' alt='instagram logo'></img>
+            <img className='logo' src={logo} alt='instagram logo'></img>
         </div>
         <div className='search'>
-            <img className='search' src='' alt='search icon'></img>
-            <input></input>
+            <img className='search' src={search} alt='search icon'></img>
+            <span>Search</span>
+            <input placeholder='search'></input>
         </div>
 
     </nav>
