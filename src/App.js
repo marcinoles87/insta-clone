1fb3a3ec820bf534399b5cf105bc84518e82d6f0
marcinoles87 <marcinoles87@gmail.com>
2023-06-24 08:38:55 +0200

    add data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
+import { useState } from 'react';
+import axios from 'axios'
 import './App.css';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 
 function App() {
+
+  const [friend , setFriend ] = useState([]);
+
+const baseURL = "https://randomuser.me/api/";
+
+
   return (
     <div className="App">
       <Navigation></Navigation>
