e83d941cfd29142004079d65ab8171b18640c379
marcinoles87 <marcinoles87@gmail.com>
2023-06-24 15:42:20 +0200

    add data

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,21 @@ import React from 'react'
 import './header.scss'
 
 function Header({friend}) {
+  const key = Math.random()*1000;
   return (
     <div className='header-container'>
         <div className='header-friend-logo'>
-          {/* {friend.map( (friend) => {
+          {friend.map( (friend) => {
             return(
               <>
-              <div> {friend.gender}</div>
+              <li key={key}> 
+              <img src={friend.picture.thumbnail} alt='friend'></img>
+              <p>{friend.id.name}</p>
+              <h2>{friend.location.country}</h2>
+              </li>
               </>
             )
-          })} */}
+          })}
         </div>
         <div className='header-main'>header-logo</div>
         <div className='header-link-right'>header-link-right</div>
