6c989329fa6b3d56c89a7397d33aea70cad458d2
marcinoles87 <marcinoles87@gmail.com>
2023-06-27 05:47:21 +0200

    add profil card

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -3,18 +3,22 @@ import React, { useEffect } from 'react'
 function Profil({friend , profil}) {
 
     console.log(friend.length)
-
-    useEffect( () => {
          const id = Math.floor(Math.random()*friend.length);
     console.log(friend[id])
 
     const myProfil = friend[id]
-    },[friend])
+
 
    
   return (
     <div>
-        <p>user name</p>
+        
+        <p>{friend.map(  (myPhoto , index) => {
+            
+            return(
+                <p>{friend[id].name.last}</p>
+            )
+        })}</p>
         <p>User City</p>
         <img src={profil} alt='profil-img'></img>
         <img src='#' alt='userPhoto'></img>
