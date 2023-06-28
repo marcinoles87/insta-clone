a642bec45ff164782f914332426883056a6945e0
marcinoles87 <marcinoles87@gmail.com>
2023-06-28 15:56:49 +0200

    add profil card

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -13,7 +13,7 @@ function Profil({friend}) {
         {selected.map(  (friend , index) => {
             return(
                 <div className="profil-card" key={index}>
-                    <p className="profil-info">{friend.name.last} {friend.name.first}</p>
+                    <p className="profil-name">{friend.name.last} {friend.name.first}</p>
                     <p className="profil-info">{friend.location.country}</p>
                     <img className="profil-photo" src={friend.picture.large} alt='photos'></img>
                 </div>
