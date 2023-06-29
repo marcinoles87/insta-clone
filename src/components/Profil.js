85ee79be32746c90972e16c747f979b7e73811d9
marcinoles87 <marcinoles87@gmail.com>
2023-06-29 05:26:05 +0200

    work with main profil

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -13,9 +13,11 @@ function Profil({friend}) {
         {selected.map(  (friend , index) => {
             return(
                 <div className="profil-card" key={index}>
+                    <img className="profil-photo" src={friend.picture.thumbnail} alt='photos'></img>
                     <p className="profil-name">{friend.name.last} {friend.name.first}</p>
                     <p className="profil-info">{friend.location.country}</p>
-                    <img className="profil-photo" src={friend.picture.large} alt='photos'></img>
+
+
                 </div>
            )
         })}
