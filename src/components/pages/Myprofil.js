4379363cde5cbca63b873d55c285004cdefeab57
marcinoles87 <marcinoles87@gmail.com>
2023-06-30 20:39:59 +0200

    work with main profil

--- src/components/pages/Myprofil.js
+++ src/components/pages/Myprofil.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Myprofil() {
   return (
-    <div>Myprofil</div>
+    <div className='myprofil-container'>
+      <img className='myprofil-photo' src="#" alt='myprofil-face'></img>
+      <div className='myprofil-name'>
+        <p>my name</p>
+        <p>second name</p>
+        <p> info</p>
+      </div>
+    
+    </div>
   )
 }
 
