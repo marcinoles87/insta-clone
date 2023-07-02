fc7479f2071fd95de2cb26d8bb74e3b71e3f26c7
marcinoles87 <marcinoles87@gmail.com>
2023-07-02 10:46:22 +0200

    work with friend profill

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -28,11 +28,13 @@ function Profil({friend , imagesprofil}) {
             return(
                 <div className="profil-card" key={index}>
                   <div className='profil-card-up'> 
-                        <img className="profil-photo" src={friend.picture.thumbnail} alt='photos'></img>
+                       
+
                             <div className='profil-card-name'>
+                            <img className="profil-photo" src={friend.picture.thumbnail} alt='photos'></img>
                               <p className="profil-name">{friend.name.last} {friend.name.first}</p>
                               <p className="profil-info">{friend.location.country}</p>
-                        </div>
+                            </div>
                          
                         
                     <img className='profil-main-photo' src={imagesprofil} alt='pd'></img>
