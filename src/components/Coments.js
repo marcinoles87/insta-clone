43548860b111dcfb78d4d588fb3ee101e3e5623d
marcinoles87 <marcinoles87@gmail.com>
2023-07-03 16:04:24 +0200

    work with comments

--- src/components/Coments.js
+++ src/components/Coments.js
@@ -2,15 +2,18 @@ import React, { useState } from 'react'
 
 function Coments({friend}) {
 
-  const [comments , setComments] = useState([])
-  const [date , setDate] = useState()
+  const [comments , setComments] = useState();
+  const [date , setDate] = useState();
+
+  const ar = []
 
  const handleOnChange = (e) =>{
   const valueInput = e.target.value;
   setComments(valueInput)
  }
    
-  const handleAddComments = () => {
+  const handleAddComments = (e) => {
+    e.preventDefault()
     const todayDate = new Date();
     const day = todayDate.getDate();
     const month = todayDate.getMonth();
@@ -18,7 +21,9 @@ function Coments({friend}) {
     const fullDate = `${day}.${month}.${year}`
     console.log(fullDate)
     setDate(fullDate)
+    ar.push(comments)
   
+    console.log(ar)
   }
   return (
     <div className='comments-container'>
@@ -26,16 +31,11 @@ function Coments({friend}) {
         <input className='comments-input' placeholder='add some comments...' onChange={handleOnChange}></input>
         <button className='comments-button-add' onClick={handleAddComments}>Add</button>
         <p>{date}</p>
-        {comments.map( (comment) => {
-          return(
-            <div>
-              <li>
-                <p>{comment}</p>
-              </li>
-              </div>
-          )
+        <p>{comments}</p>
+        <p>{ar}</p>
+        
 
-        })}
+        
     </div>
   )
 }
