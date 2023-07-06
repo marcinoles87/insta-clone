4648c03e9e7068977de1b914b414d326e3792370
marcinoles87 <marcinoles87@gmail.com>
2023-07-06 05:20:23 +0200

    work with comments

--- src/components/pages/Myprofil.js
+++ src/components/pages/Myprofil.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './myprofil.scss'
 
 function Myprofil({friend}) {
+
+  const followers = (Math.random()*10000).toPrecision(7);
   
   const my = friend.slice(0,1)
 
@@ -11,9 +13,10 @@ function Myprofil({friend}) {
       return(
       <div key={index} className='myprofil-container'>
       <img className='myprofil-photo' src={item.picture.large} alt='myprofil-face'></img>
-      <div className='myprofil-name'>
-        <p>{item.name.first}</p>
-        <p>{item.name.last}</p>
+      <div className='myprofil-info'>
+        <p className='myprofil-name'>{item.name.first} {item.name.last}</p>
+        <p className='myprofil-country'>{item.location.country}</p>
+        <p className='myprofi-fallowers'>{followers} followers</p>
         
         <p> info</p>
         <div className='profil-gallery'>
